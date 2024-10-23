c1182fbb4b32bfacf636409c8c4b308d197dba48
Iñigo Esteban <nack_z4@hotmail.com>
2024-10-23 13:07:57 +0200

    basic styles and hover

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,19 @@
 <template>
   <div>
-    <header class="header">
-      <img 
-      class="logo-roy"
-      src="../images/Title.png" 
-      alt="roy">
+    <header>
+      <a href="#">
+        <img class="logo-roy" src="../images/Title.png" alt="Roy´s homepage">
+      </a>
     </header>
     <div class="blank-space"></div>
     <main>
+      <h1 class="sr-only">Homepage</h1>
       <section>
-        <div class="content">
-          <div class="content-info">
-            <span>YACHTS FOR SALE · </span><span>340</span>
-          </div>
-          <div class="content-view-mode">
-            <span>|</span>
-            <span>View</span>
+        <h2 class="sr-only">Yacht search results</h2>
+        <div class="counter-results-and-layout-actions">
+          <p>YACHTS FOR SALE · 340</p>
+          <div class="actions">
+            <span>|  View</span>
             <a class="icon-grid" href="#">
               <img src="../images/grid.png" alt="grid-items">
             </a>
@@ -24,28 +22,57 @@
             </a>
           </div>
         </div>
-        <article class="card-group">
-          <div class="card">
+        <div class="yacht-cards">
+          <article class="yacht-card">
             <img class="img"
               src="https://www.arthaudyachting.com/wp-content/uploads/2023/02/Yacht-charter-M-Y-DOMINIQUE_8.jpg"
-              alt="yatch">
-            <div class="card-info">
-              <p class="price">Price: <span>€2.000.000</span></p>
-              <span>Length: <span>55.2m</span></span>
-              <span> | </span>
-              <span>Guests: <span>12</span></span>
-              <span> | </span>
-              <span>Cabins: <span>7</span></span>
+              alt="yacht image">
+              <a href="#" class="fav" >
+                <img src="../images/fav.icon.png" alt="fav icon">
+              </a>
+            <div class="details">
+              <p class="price">Price: €2.000.000</p>
+              <p>Length: 55.2m | Guests: 12 | Cabins: 7</p>
             </div>
-            <div class="more-details">
-              <h3>Yatch Name</h3>
-              <button class="btn --dark">Enquiry</button>
+            <div class="header-and-enquire-action">
+              <h3>Yacht Name</h3>
+              <a class="enquiry" href="#" aria-label="Enquire Yacht Name">Enquiry</a>
             </div>
-          </div>
-          <button class="btn-light">
-            Load more
-          </button>
-        </article>
+          </article>
+          <!--  -->
+          <article class="yacht-card">
+            <img class="img"
+              src="https://www.arthaudyachting.com/wp-content/uploads/2023/02/Yacht-charter-M-Y-DOMINIQUE_8.jpg"
+              alt="yacht image">
+            <div class="details">
+              <p class="price">Price: €2.000.000</p>
+              <p>Length: 55.2m | Guests: 12 | Cabins: 7</p>
+            </div>
+            <div class="header-and-enquire-action">
+              <h3>Yacht Name</h3>
+              <a class="enquiry" href="#" aria-label="Enquire Yacht Name">Enquiry</a>
+            </div>
+          </article>
+          <!--  -->
+          <article class="yacht-card">
+            <img class="img"
+              src="https://www.arthaudyachting.com/wp-content/uploads/2023/02/Yacht-charter-M-Y-DOMINIQUE_8.jpg"
+              alt="yacht image">
+            <div class="details">
+              <p class="price">Price: €2.000.000</p>
+              <p>Length: 55.2m | Guests: 12 | Cabins: 7</p>
+            </div>
+            <div class="header-and-enquire-action">
+              <h3>Yacht Name</h3>
+              <a class="enquiry" href="#" aria-label="Enquire Yacht Name">Enquiry</a>
+            </div>
+          </article>
+        </div>
+        <div class="footer">
+          <button class="load-button">
+              Load more
+            </button>
+        </div>
       </section>
     </main>
   </div>
@@ -90,25 +117,28 @@ export default Vue.extend({
 *::before, *::after {
   display: block;
 }
+
 :root {
-  /* COLOURS */
+  /* COLORS */
   --color-white: #FFFFFF;
-  --color-grey-oceanLux-800: #323349;
-  --color-grey-oceanLux-600: #676778;
-  --color-grey-price: #858585;
-  --color-grey-info: #4e4e4e;
-  --color-black-title: #102D2D;
-  --color-black-900-name: #091919;
-  --color-black-800-button: #0B2020;
-  --color-grey-blue-500-button: #405757;
-  --color-transparent: #00000000;
+  --color-oceanLux-800: #323349;
+  --color-oceanLux-600: #676778;
+  --color-oceanLux-400: #676778;
+  --color-black-scale-500: #858585;
+  --color-black-scale-700: #4E4E4E;
+  --color-black-0B2020: #0B2020;
+  --color-primary-900: #091919;
   /* FONTS */
-  --font-family-texts:  UrbanGroteskReBo, Helvetica, Arial, sans-serif;
+  --font-family: UrbanGroteskReBo, Helvetica, Arial, sans-serif;
   --font-weight-regular: 400;
-  --font-size-16: 1rem;
-  --font-size-20: 1.25rem;
+
+  --font-h3: var(--font-weight-regular) 20px/1.5 var(--font-family);
+  --font-body-20: var(--font-weight-regular) 20px/1.5 var(--font-family);
+  --font-body-16: var(--font-weight-regular) 16px/1.5 var(--font-family);
+  --font-button-16: var(--font-weight-regular) 16px/1.5 var(--font-family);
 
   /* SPACINGS */
+  --spacing-2: 0.125rem;
   --spacing-4: 0.25rem;
   --spacing-8: 0.5rem;
   --spacing-10: 0.625rem;
@@ -117,77 +147,139 @@ export default Vue.extend({
   --spacing-24: 1.5rem;
   --spacing-32: 2rem;
   --spacing-40: 2.5rem;
+  --spacing-48: 3rem;
+  --spacing-60: 3.75rem;
   --spacing-64: 4rem;
-  --zindex-header: 100;
-  /* BORDERS */
-  --border-radius-4: 0.25rem;
-  --border-width-1: 0.0625rem;
-  --input-and-btn-height: 2.75rem;
+}
+
+body {
+  /* default font styles */
+  color: var(--color-black-700);
+  font: var(--font-body-16);
+}
+h3{
+  font: var(--font-h3);
 }
 /* HEADER */
-.blank-space{
+.blank-space {
   padding-bottom: var(--spacing-32);
 }
-.header{
-display: flex;
-justify-content: center;
-align-items: center;
-padding-top: var(--spacing-12);
-padding-bottom: var(--spacing-12);
+
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: var(--spacing-12);
+  padding-bottom: var(--spacing-12);
 }
-.logo-roy{
+
+.logo-roy {
   width: 3.875rem; /*62px*/
   height: 1.75rem; /*28px*/
 }
+
 /* MAIN CONTENT */
-.content{
-  font-family: var(--font-family-texts);
+.counter-results-and-layout-actions {
   display: flex;
   justify-content: space-between;
   padding: var(--spacing-8) var(--spacing-16) var(--spacing-8) var(--spacing-16);
+  font: var(--font-body-20);
+  color: var(--color-oceanLux-800);
 }
-.grid-items, .solo-items{
-  width: 1.5rem;
-  height: 1.5rem;
-}
-.content-info{
+.actions{
   display: flex;
   justify-content: center;
   align-items: center;
   gap: var(--spacing-8);
-  font-size: var(--font-size-20);
-  color: var(--color-grey-oceanLux-800);
+  color: var(--color-oceanLux-400);
+  font: var(--font-body-16);
 }
-.content-view-mode{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: var(--spacing-8);
-  color: var(--color-grey-oceanLux-600);
-  font-size: var(--font-size-16);
-}
-.card-group{
+
+.yacht-cards {
   padding-left: var(--spacing-16);
   padding-right: var(--spacing-16);
-}
-.card {
-  width: 21.875rem; /*350px*/
-  height: 21.25rem; /*340px*/
-}
-.img {
-  width: 21.875rem; /*350px*/
-  height: 13.75rem; /*220px*/
-}
-.card-info{
-  font-family: var(--font-family-texts);
-  color: var(--color-grey-info);
-  gap: 2px;
-
-}
-.price{
-font-family: var(--font-family-texts);
-font-size: var(--font-size-16);
-color: var(--color-grey-price);
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+  gap: var(--spacing-48) var(--spacing-2);
 }
 
+.yacht-card {
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-12);
+  color: var(--color-black-scale-700);
+  position: relative;
+}
+
+.fav{
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  z-index: 100;
+  width: 26px;
+  height: 26px;
+}
+
+.price {
+  color: var(--color-black-scale-500);
+}
+
+.header-and-enquire-action{
+  display: flex;
+  justify-content: space-between;
+  padding-right: var(--spacing-16);
+}
+
+.enquiry{
+background-color: var(--color-black-0B2020);
+color: var(--color-white);
+text-decoration: none;
+display: flex;
+align-items: center;
+justify-content: center;
+width: 88px;
+height: 44px;
+padding: var(--spacing-8) var(--spacing-16) var(--spacing-8) var(--spacing-16);
+opacity: 0;
+}
+
+.fav, .enquiry {
+  opacity: 0;
+  transition: opacity 0.3s ease;
+}
+
+.yacht-card:hover .fav,
+.yacht-card:hover .enquiry {
+  opacity: 1;
+}
+
+/* FOOTER */
+
+.footer{
+display: flex;
+justify-content: center;
+padding-top: var(--spacing-64);
+padding-bottom: var(--spacing-64);
+}
+.load-button{
+  width: 6.875rem; /* 110px */
+  height: 2.75rem; /* 44px */
+  background-color: transparent;
+  border: 1px solid var(--color-oceanLux-600);
+  border-radius: 0.125rem;
+  color: var(--color-oceanLux-600);
+}
+
+/*UTILS*/
+.sr-only {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  padding: 0;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  border: 0;
+  padding: var(--spacing-8) var(--spacing-16) var(--spacing-8) var(--spacing-16);
+}
 </style>
